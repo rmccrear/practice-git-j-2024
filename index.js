47462f7e8a5bcdb67325baa46ccb0c007da0528b
Robert McCreary Mannino <fooyinfighter@gmail.com>
2024-03-04 18:30:03 -0800

    practice with for loops

---
+++ index.js
@@ -0,0 +1,57 @@
+let cars = ["ford", "pontiac", "mazda", "toyota", "jeep"];
+
+for(let i=0; i<5; i++) {
+  console.log(`Your car is ${cars[i]}`);
+}
+
+let teams = ['steelers', 'cowboys', 'eagles', 'titans'];
+
+// for(let i=0; i<4; i++) {
+//   conso ${}teams[]i.
+
+  
+let food = ["burgers", "pasta", "rice", "pasta"];
+for(let i=0; i<=3; i++) {
+    console.log(`These are types of ${food[i]}`);
+}
+
+
+
+let shoes = ["reebook", "nike", "adidas", "puma"]
+for (let t=0; t<5; t++){
+    console.log(`types off shoes are ${shoes[t]}`);
+}
+
+let goat = ["jordan", "lebron", "magic", "kobe"]
+// console.log(goat[0]);
+// console.log(goat[1]);
+// console.log(goat[2]);
+// console.log(goat[3]);
+for ( let g=0; g<4; g++) {
+    console.log(`who is the goat ${goat[g]}`);
+}
+
+// for(let i=0; i<3; i++) {
+//   console.log(`${i} cans of soup.`);
+// }
+
+// for(let i=0; i<3; i++) {
+//   console.log(`${i} steelers' fans.`);
+// }
+
+// for(let b=0; b<7; b++) {
+//   console.log(`${b} finals championships`);
+// }
+
+// for(let w=0; w<3; w++) {
+//   console.log(`${w} superbowl wins`);
+// }
+
+
+// for(let t=0; t<10; t++) {
+//     console.log(`${t} shirts`);
+// } 
+
+
+
+
